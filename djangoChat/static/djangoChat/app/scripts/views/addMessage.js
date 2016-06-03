9b68e3dca0e6a5d5b5819ef8a730d3f1bb813360
Nyeint Nyeint Khin <nyeintnyeintkhin.92@gmail.com>
2016-06-03 09:44:34 +0630

    - changing console msg

--- djangoChat/static/djangoChat/app/scripts/views/addMessage.js
+++ djangoChat/static/djangoChat/app/scripts/views/addMessage.js
@@ -9,12 +9,15 @@ define(['backbone','channel'],function(Backbone,Channel){
 
 		submit : function(){
 			nwmsg = this.$el.find('textarea').val();
+			console.log("nwmsg" + nwmsg);
+
 			this.$el.find('textarea').val('');
 			Channel.trigger('addMsg',nwmsg); // reciever messages.view
 			//auto_scroll();
 		},
 		submitOnEnter:function(e){
-			console.log(e.keyCode);
+			//console.log(e.keyCode);
+			console.log("Enter key:" + e.keyCode);
 			if(e.keyCode===13 && !e.shiftKey){
 				this.submit();
 			}
